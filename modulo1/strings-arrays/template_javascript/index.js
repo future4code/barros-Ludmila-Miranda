399c80da44bb1e90821ef30d1182e0c9660c1c58
ludlulk <ludlulk@gmail.com>
2022-04-19 22:57:07 -0300

    terminar exercícios interpretativos

--- modulo1/strings-arrays/template_javascript/index.js
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -1,23 +1,33 @@
-//let array
+///let array
 //console.log('a. ', array)
-// 
+//  retorna undefined   
 
 // array = null
 //console.log('b. ', array)
+// retorna null
 
 // array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 // console.log('c. ', array.length)
+// retorna 11
 
-// let i = 0
-// console.log('d. ', array[i])
+
+ //let i = 0
+ //console.log('d. ', array[i])
+ // dá erro por conceito de variável let
 
 // array[i+1] = 19
 // console.log('e. ', array)
+// dá erro por conceito de variável 
 
 // const valor = array[i+6]
 // console.log('f. ', valor)
+// dá erro por conceito de variável const
 
 
-const frase = prompt("Digite uma frase")
+//const frase = prompt("Digite uma frase")
 
-console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+//console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+// SUBI NUM ONIBUS EM MIRROCOS
+
+// ---- EXERCÍCIOS INTERPRETATIVOS -----
