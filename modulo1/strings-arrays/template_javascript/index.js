d154e6aded8e2fd8291bd9e84200ef2ea818a9b2
ludlulk <ludlulk@gmail.com>
2022-04-19 22:09:49 -0300

    exercício 2]

--- modulo1/strings-arrays/template_javascript/index.js
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -2,21 +2,22 @@
 //console.log('a. ', array)
 // 
 
-array = null
-console.log('b. ', array)
+// array = null
+//console.log('b. ', array)
 
-array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-console.log('c. ', array.length)
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length)
 
-let i = 0
-console.log('d. ', array[i])
+// let i = 0
+// console.log('d. ', array[i])
 
-array[i+1] = 19
-console.log('e. ', array)
+// array[i+1] = 19
+// console.log('e. ', array)
 
-const valor = array[i+6]
-console.log('f. ', valor)
+// const valor = array[i+6]
+// console.log('f. ', valor)
 
 
-// Exercício 2 não vai se alterar porque quando -const- não permite alterar a variável.
-// portanto não se altera.
+const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
