a2e847b021f49f454f98e2680c501ea32590149a
ludlulk <ludlulk@gmail.com>
2022-04-19 21:31:35 -0300

    exercicio1

--- modulo1/strings-arrays/template_javascript/index.js
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -1,4 +1,17 @@
-const frase = prompt("Digite sua frase")
+let array
+console.log('a. ', array)
 
+array = null
+console.log('b. ', array)
 
-console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+
+let i = 0
+console.log('d. ', array[i])
+
+array[i+1] = 19
+console.log('e. ', array)
+
+const valor = array[i+6]
+console.log('f. ', valor)
