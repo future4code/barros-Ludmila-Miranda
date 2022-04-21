62f1ca15b3567ca1ccd04909ee6b97c85af6c6d8
ludlulk <ludlulk@gmail.com>
2022-04-21 18:13:36 -0300

    exercicio imterpretacao e codigo

---
+++ modulo1/funcoes/template_javascript/index.js
@@ -0,0 +1,77 @@
+let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)
+
+    // 1)// primeira impressão retornará 10
+// segunda impressão retornará 50
+
+2 // a) função coloca tudo em maiúscula e procura se a afirmação
+//possui a palavra "cenoura"
+///b) As três retornam TRUE
+
+// EXERCICIOS DE ESCRITA //
+
+
+
+//a)
+`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.`
+
+//2 a) 
+var numero1 = 0
+var numero2 = 0
+
+function soma (numero1,numero2){return  numero1 + numero2}
+soma (3,5)
+8
+
+//b)
+ var numero1 = 4 
+var numero2 = 6
+
+function maior(numero1,numero2){return numero1 > numero2}
+ maior(numero1,numero2)
+
+//c) ----
+
+//d)
+function msg (mensagem){var len = mensagem.length; return mensagem.toUpperCase() + len}
+
+function msg (mensagem){var len = mensagem.length; return mensagem.toUpperCase() + " " + len}
+
+msg(mensagem)
+'LILI QUER BRINCAR 17'
+
+
+//3)
+ let textoDoUsuario1 = prompt("Insira o primeiro número") //5
+
+let textoDoUsuario2 = prompt("Insira o segundo número")// 6
+
+function soma (textoDoUsuario1,textoDoUsuario2){return numero1 + numero2}
+
+function divisão (textoDoUsuario1,textoDoUsuario2){return numero1 / numero2}
+
+function multiplicação (textoDoUsuario1,textoDoUsuario2){return numero1 * numero2}
+
+function subtração (textoDoUsuario1,textoDoUsuario2){return numero1 - numero2}
+
+soma(textoDoUsuario1,textoDoUsuario2)
+
+11
+
+divisão(textoDoUsuario1,textoDoUsuario2)
+
+0,833333333334
+
+multiplicação(textoDoUsuario1,textoDoUsuario2)
+
+30
+
+subtração(textoDoUsuario1,textoDoUsuario2)
+
+-1
