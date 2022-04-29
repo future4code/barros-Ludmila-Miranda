2b5747f577ad5bf6213a6cc54347f56bcc93eb17
ludlulk <ludlulk@gmail.com>
2022-04-28 21:02:04 -0300

    exercício de interpretação

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,14 @@
+/// exercicio de interpretação 1 ////
+/// o número testado foi 3 
+/// o resultado foi 'não passou no teste'
+/// após foi testado o número 4
+/// resultado 'passou no teste'
+
+// a) o código testa se o número é divisível ou multiplicador de 2
+
+// b) ele imprime no console 'passou no teste' números divisíveis por 2 sem resto e diferentes de 0
+/// c) ele imprime no console 'não passou no teste' números que quando divisível por 2 dá resto
+
+/// exercício de interpretação 2 ////
+
+//
