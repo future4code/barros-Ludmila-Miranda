2b127ca7dd3973266505e99cbe21816806d91126
ludlulk <ludlulk@gmail.com>
2022-05-05 21:14:08 -0300

    exercício 2

--- modulo1/callback/index.js
+++ modulo1/callback/index.js
@@ -11,4 +11,14 @@
   // resposta exercício um interpretação a) vai ser impresso no console os arrays com o nome, apelido e
   // o índice que essa posição ocupa. (0,1,2)
 
+  const usuarios = [
+    { nome: "Amanda Rangel", apelido: "Mandi" },
+    { nome: "Laís Petra", apelido: "Laura" },
+    { nome: "Letícia Chijo", apelido: "Chijo" },
+  ]
   
+  const novoArrayB = usuarios.map((item, index, array) => {
+     return item.nome
+  })
+  
+  console.log(novoArrayB)
