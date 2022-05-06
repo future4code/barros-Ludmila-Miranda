86b817c141f6217723ed9aefac338ff80f8451ea
ludlulk <ludlulk@gmail.com>
2022-05-05 21:12:58 -0300

    exercício

--- modulo1/callback/index.js
+++ modulo1/callback/index.js
@@ -1,9 +1,14 @@
-const usuarios = [
-    { nome: "Amanda Rangel", apelido: "Mandi" },
-    { nome: "Laís Petra", apelido: "Laura" },
-    { nome: "Letícia Chijo", apelido: "Chijo" }
-  ]
+//const usuarios = [
+  //  { nome: "Amanda Rangel", apelido: "Mandi" },
+   // { nome: "Laís Petra", apelido: "Laura" },
+    //{ nome: "Letícia Chijo", apelido: "Chijo" }
+  //]
   
-  const novoArrayA = usuarios.map((item, index, array) => {
-     console.log(item, index, array)
-  })
+  //const novoArrayA = usuarios.map((item, index, array) => {
+    // console.log(item, index, array)
+  //})
+
+  // resposta exercício um interpretação a) vai ser impresso no console os arrays com o nome, apelido e
+  // o índice que essa posição ocupa. (0,1,2)
+
+  
